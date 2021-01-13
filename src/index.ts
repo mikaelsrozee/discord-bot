6e1b3a7c3d03238ecd48563f9bda3ed09a7ec763
mikaelsrozee <mikaelsrozee@gmail.com>
2021-01-13 20:12:20 +0000

    create express so that heroku doesn't cry

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import {Client} from 'discord.js'
 import {processMessage} from './commandHandler'
+import express from 'express'
+
+const app = express()
 
 require('dotenv').config()
 
@@ -13,3 +16,7 @@ function addEventListeners() {
 addEventListeners()
 
 client.login(process.env.BOT_TOKEN)
+
+app.get('/', (_, res) => res.sendStatus(200))
+
+app.listen(process.env.PORT || 8080)
